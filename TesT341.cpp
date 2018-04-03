8a0c4484dc0964efc8229a8e107099936b388752
TheBlueWhieWhale <38017271+TheBlueWhieWhale@users.noreply.github.com>
2018-04-02 22:49:19 -0500

    Update TesT341.cpp
    Updated it to have less than half the code it did previously

--- TesT341.cpp
+++ TesT341.cpp
@@ -1,5 +1,26 @@
 //This is a test, the code doesn't work
 //Correction, now it does. You're welcome, Alexander.
+//========================
+//You sure this works?? I doubt it will.
+//It does, but I remade it better anyways
+//old version you made at the bottom for comparison btw, I made mine in 9 lines compared to your 20 lines
+//and thats with mine being more spaced out than yours, Samuel
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int ChosenOne = 0;
+    cout <<"Am I brain dead?!?\n1=Yes 2=No\nAnswer: ";
+    cin>>ChosenOne;
+    
+    if(ChosenOne==1)
+      cout<<"I'm dead...";
+    else
+    cout<<"meow";
+    
+}
+/*
 #include <iostream>
 using namespace std;
 int dead = 0;
@@ -22,3 +43,4 @@ if(mark==dead)
  else
   printf("meow");
 }
+*/
